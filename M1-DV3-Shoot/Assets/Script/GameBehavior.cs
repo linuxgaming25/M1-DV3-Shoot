4bf45244dfbeaf0175278f5c673ae150e80a7f89
linuxgaming25 <127547557+linuxgaming25@users.noreply.github.com>
2024-02-15 22:51:26 -0700

    Fixed code

--- M1-DV3-Shoot/Assets/Script/GameBehavior.cs
+++ M1-DV3-Shoot/Assets/Script/GameBehavior.cs
@@ -18,6 +18,8 @@ public class GameBehavior : MonoBehaviour
         get { return _itemsCollected; }
 
         set { _itemsCollected = value;
+            Debug.LogFormat("Items: {0}", _itemsCollected);
+
             if(_itemsCollected >= maxItems)
             {
                 labelText = "You've found all the items!";
@@ -31,7 +33,7 @@ public class GameBehavior : MonoBehaviour
         }
     }
 
-    private int _playerHP = 3;
+    private int _playerHP = 10;
     public int HP
     {
         get { return _playerHP; }
@@ -41,6 +43,10 @@ public class GameBehavior : MonoBehaviour
     }
     void OnGUI()
     {
+        GUI.Box(new Rect(20, 20, 150, 25), "Player Health:" + _playerHP);
+        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
+
         if(showWinScreen)
         {
             if(GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 - 50, 200, 100), "YOU WON!"))
